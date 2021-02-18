293974e1575e71e48d31fc03a2e221bee1e2b270
Domantas <domantasslai@gmail.com>
2021-02-18 20:44:51 +0200

    Constants

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-<h1>
-    <?php
-        'Hello from insidess';
-    ?>
-</h1>
+// Constants
 
-</body>
-</html>
+define('STATUS_PAID', 'paid');
+
+if(defined('STATUS_PAID122')){
+    echo 'Defined';
+} else {
+    echo 'Not Defined';
+}
+
+echo '<br>';
+
+const STATUS_NOT_PAID = 'not payed';
+
+echo STATUS_NOT_PAID;
+echo '<br>';
+echo STATUS_PAID;
